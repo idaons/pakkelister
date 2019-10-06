63ccab9e90e85f3c645f6afa2dd344c46bb92e91
Ida Onshus <idaonshus@gmail.com>
2019-10-06 15:23:40 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import "./styles.css";
+
+interface Props {
+   name: string
+}
+
+class App extends React.Component<Props> {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
