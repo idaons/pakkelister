b3da54b813106fc25ea1143e070f8be0caefbdb4
Ida Onshus <idaonshus@gmail.com>
2020-01-18 16:18:21 +0100

    styling på checkbokser

--- src/pakk/Pakk.tsx
+++ src/pakk/Pakk.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { AppStages, AppStateContext } from '../app/appStateContext';
 import { pakkAlleLister } from './listMakers/allRules';
 import { groupArray } from '../utils/groupArray';
 import { Kategori } from '../models/kategori';
-import Checkbox from "../utils/baseComponents/Checkbox";
 import classes from './pakk.less';
+import { guid } from "../utils/guid";
+import { InputProps } from "../utils/baseComponents/Input";
+
+
+function Checkbox(props: InputProps) {
+    const ref = useRef(guid());
+    const { label, ...rest } = props;
+    return (
+        <div className={classes.input}>
+            <input id={ref.current} type='checkbox' {...rest} />
+            <label htmlFor={ref.current}>{label}</label>
+        </div>
+    );
+}
 
 function Pakk() {
     const context = useContext(AppStateContext);
@@ -30,7 +43,9 @@ function Pakk() {
                                     <Checkbox
                                         key={'chk-'+element.navn}
                                         label={(element.antall > 1 ? element.antall + ' ': '') + element.navn}
+
                                     />
+
                                 </li>
                             ))}
                         </ul>
