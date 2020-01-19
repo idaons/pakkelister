570ad47fe5e118ef9f68dd0c71d0f52c5b53e0cf
Ida Onshus <idaonshus@gmail.com>
2020-01-19 17:14:29 +0100

    progresjonsbar

--- src/pakk/Pakk.tsx
+++ src/pakk/Pakk.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { pakkAlleLister } from './listMakers/allRules';
 import { groupArray } from '../utils/groupArray';
 import { Kategori } from '../models/kategori';
@@ -8,12 +9,11 @@ import { decodeUrlParams, valgToUrlParams } from '../utils/valgToUrlParams';
 import LinkButton from '../utils/baseComponents/LinkButton';
 
 
+
+
 function Pakk(props: { urlValg: string }) {
     const valg = decodeUrlParams(props.urlValg);
-
-    const tilbakeKnapp = (
-        <LinkButton to={'/nyliste/' + valgToUrlParams(valg.valg)}>Tilbake</LinkButton>
-    );
+    const [checkedItems, setCheckedItems] = useState<String[]>([]);
 
     if (valg.feilmelding) {
         return (
@@ -24,13 +24,30 @@ function Pakk(props: { urlValg: string }) {
         );
     }
 
+    const updateCheckedItems = (e : React.ChangeEvent<HTMLInputElement> ) => {
+        const value = e.target.value;
+        console.log(e.target);
+        if (e.target.checked) {
+            setCheckedItems([...checkedItems, value]);
+        } else {
+            setCheckedItems(checkedItems.filter(a => a !== value));
+        }
+    };
+
     const alleElementer = pakkAlleLister(valg.valg);
     const iKategorier = groupArray(alleElementer, it => Kategori[it.kategori]);
+    const progress = Math.floor((checkedItems.length * 100) / alleElementer.length);
+    document.documentElement.style.setProperty('--progress', progress+ '%');
 
     return (
         <div className={classes.pakk}>
-            {tilbakeKnapp}
-            Du må pakke!
+            <LinkButton to={'/nyliste/' + valgToUrlParams(valg.valg)}>Tilbake</LinkButton>
+
+            <div className={classes.progress}>
+                 <i className={classes.icon}></i>
+                 <span className={classes.prosent}>{progress}%</span>
+            </div>
+
             <ul>
                 {iKategorier.map(kategori => (
                     <div key={kategori.category}>
@@ -40,8 +57,10 @@ function Pakk(props: { urlValg: string }) {
                                 <li key={element.navn}>
                                     <Checkbox
                                         key={'chk-'+element.navn}
+                                        value={element.navn}
                                         label={(element.antall > 1 ? element.antall + ' ': '') + element.navn}
                                         strikeThrough={true}
+                                        onChange={updateCheckedItems}
                                     />
                                 </li>
                             ))}
