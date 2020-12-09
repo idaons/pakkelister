c6a0900a4fdebdf821b5d628e3980ca77a7f82b1
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-09 09:25:36 +0100

    prøver å fikse redirect-bug, dersom første redirect ikke funker sendes man bare til forside

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import LagListe from '../lagListe/LagListe';
 import Pakk from '../pakk/Pakk';
 import { Redirect, Router } from '@reach/router';
+import { useState, useRef } from 'react';
 
 export const basepath = '/pakkelister';
 
 const HandleRedirect = (props: { default: boolean }) => {
+    const firstRedirect = useRef(true);
+
     const redirect = window.localStorage.getItem('redirect');
     window.localStorage.removeItem('redirect');
-    return <Redirect noThrow from={'/'} to={redirect || `${basepath}/urlParams`} />;
+
+    const to = (firstRedirect.current && redirect) || `${basepath}/urlParams`;
+    firstRedirect.current = false;
+
+    return <Redirect noThrow from={'/'} to={to} />;
 };
 
 function App() {
@@ -20,4 +27,5 @@ function App() {
         </Router>
     );
 }
+
 export default App;
