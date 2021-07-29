2eff85ccd3ab45633c5b544d4f80744acb30c7e7
winsvold <29202094+winsvold@users.noreply.github.com>
2021-07-29 21:01:36 +0200

    migrerer mye styling til styled components

--- src/utils/baseComponents/InputBase.tsx
+++ src/utils/baseComponents/InputBase.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { InputHTMLAttributes, useRef } from 'react';
 import { guid } from '../guid';
-import * as classes from './input.less';
+import styled, {css} from "styled-components";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
@@ -9,19 +9,53 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     header?: boolean;
 }
 
+const Style = styled.div<{strikeThrough?: boolean, header?: boolean}>`
+  display: inline-block;
+  label {
+    padding-left: .5rem;
+    font-size: 1.2rem;
+  }
+
+  input {
+    position: relative;
+    top: -.15rem;
+    transform: scale(1.2);
+  }
+
+  transition: transform .2s;
+  &:hover {
+    transform: scale(1.1);
+  }
+  
+    ${p => p.strikeThrough && css`
+      input:checked + label {
+        text-decoration: line-through;
+        color: rgba(136, 136, 136, 0.98);
+      }
+    `}
+    
+    ${p => p.header && css`
+      input {
+        transform: scale(1.7);
+      }
+      label {
+        padding-left: 1em;
+      }
+      h2 {
+        display: inline-block;
+      }
+    `}
+`;
+
 function InputBase(props: InputProps) {
     const ref = useRef(guid());
-    const { label, strikeThrough, header, ...rest } = props;
+    const { label, strikeThrough, header, className, ...rest } = props;
 
-    const classNames = [
-        strikeThrough ? classes.strikeThrough : '',
-        header ? classes.header : '',
-    ].join(' ');
     return (
-        <div className={`${classes.input} ${classNames}`}>
+        <Style strikeThrough={strikeThrough} header={header} className={className}>
             <input id={ref.current} {...rest} />
             <label htmlFor={ref.current}>{props.header ? <h2>{label}</h2> : label}</label>
-        </div>
+        </Style>
     );
 }
 
--- src/utils/baseComponents/TextInput.tsx
+++ src/utils/baseComponents/TextInput.tsx
@@ -1,23 +1,41 @@
 import * as React from 'react';
 import { InputHTMLAttributes, useRef } from 'react';
 import { guid } from '../guid';
-// @ts-ignore
-import * as classes from './input.less';
+import styled from "styled-components";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
     className?: string;
 }
 
+const Style = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    line-height: 1.3rem;
+    max-width: 30rem;
+    background-color: inherit;
+    color: inherit;
+    border: currentColor solid;
+    padding: .5em .75em;
+    border-radius: .25em;
+  }
+
+  label {
+    margin-bottom: 0.5rem;
+  }
+`;
+
 function TextInput(props: InputProps) {
     const ref = useRef(guid());
     const { label, className, ...rest } = props;
 
     return (
-        <div className={`${classes.textInput} ${className}`}>
+        <Style className={className}>
             <label htmlFor={ref.current}>{label}</label>
             <input type="text" id={ref.current} {...rest} />
-        </div>
+        </Style>
     );
 }
 
