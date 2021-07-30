1486545f4907e6f36ac0953dd3509e93f74f0d93
winsvold <danielwinsvold@gmail.com>
2021-07-30 19:57:33 +0000

    Deploying to gh-pages from  @ fde58dbdc6540665a466ae3996192e93c6773d01 🚀

---
+++ _next/static/chunks/pages/index-c5d3a25a862ec838bd17.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5301:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(4642)}])}},function(n){n.O(0,[910,565,774,888,179],(function(){return _=5301,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
+//# sourceMappingURL=index-c5d3a25a862ec838bd17.js.map
