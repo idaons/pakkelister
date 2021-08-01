18b73a51eaa3afcfe84762efd5442a2817c5bebf
Ida Onshus <25682301+idaons@users.noreply.github.com>
2021-08-01 16:12:19 +0200

    fikser 404side

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 
 const Style = styled.div`
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  row-gap: 1rem;
 `;
 
 export default function NotFound() {
   return (
     <Style>
-      <span>Not found 🤷‍♀</span>
-      <LinkButton href="/">Gå til startside</LinkButton>️
+      <h1>Ingenting her... 🤷‍♀</h1>
+      <LinkButton href="/">Gå til forsiden</LinkButton>️
     </Style>
   );
 }
