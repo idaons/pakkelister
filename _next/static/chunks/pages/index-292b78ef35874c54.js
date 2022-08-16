00ef80f68426a0a571318e86e609c54d5d4fc4b9
idaons <25682301+idaons@users.noreply.github.com>
2022-08-16 15:46:27 +0000

    Deploying to gh-pages from  @ c8db08e0f15615400d59adc82fdff3d47d38d685 🚀

---
+++ _next/static/chunks/pages/index-292b78ef35874c54.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(2902)}])}},function(n){n.O(0,[664,349,363,774,888,179],(function(){return _=8312,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
+//# sourceMappingURL=index-292b78ef35874c54.js.map
